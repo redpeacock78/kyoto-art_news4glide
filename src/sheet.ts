cd318f4ada789069745b0bd30c75e4913104d902
redpeacock78 <scarletpeacock78@gmail.com>
2020-11-11 21:45:29 +0900

    Change content: src/sheet.ts

--- src/sheet.ts
+++ src/sheet.ts
@@ -1,3 +1,6 @@
-const sheet = (spread_id: string, sheet_id: string): GoogleAppsScript.Spreadsheet.Sheet => {
+const sheet = (
+  spread_id: string,
+  sheet_id: string
+): GoogleAppsScript.Spreadsheet.Sheet => {
   return SpreadsheetApp.openById(spread_id).getSheetByName(sheet_id);
 };
