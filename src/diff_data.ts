e6b28744c7e6f5bebf46e73f41f33c10af617603
redpeacock78 <scarletpeacock78@gmail.com>
2020-11-20 12:01:05 +0900

    Change content: src/diff_data.ts

--- src/diff_data.ts
+++ src/diff_data.ts
@@ -2,20 +2,20 @@ function diff_data(
   first_data: string[][],
   second_data: string[][]
 ): string[][] {
-  const first_tit: string[] = first_data.map((i: string[]): string => {
-    return i[0];
+  const first_url: string[] = first_data.map((i: string[]): string => {
+    return i[1];
   });
-  const second_tit: string[] = second_data.map((i: string[]): string => {
-    return i[0];
+  const second_url: string[] = second_data.map((i: string[]): string => {
+    return i[1];
   });
 
-  const tit_diff: string[] = second_tit.filter(
-    (i: string): boolean => first_tit.indexOf(i) == -1
+  const url_diff: string[] = second_url.filter(
+    (i: string): boolean => first_url.indexOf(i) == -1
   );
 
   const diff = [];
-  for (let i = 0; i < tit_diff.length; i++) {
-    const num: number = second_tit.indexOf(tit_diff[i]);
+  for (let i = 0; i < url_diff.length; i++) {
+    const num: number = second_url.indexOf(url_diff[i]);
     diff[i] = second_data[num];
   }
 
